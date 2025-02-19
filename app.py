fb2ed27b3a7677ab56eb681e6e4f55a3aef89e45
Anidadsai <ani.da.ds.ai@gmail.com>
2025-02-19 19:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import random
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
