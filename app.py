20b352fbcb11c6fa500037fa1bdfe011c569fb16
Anidadsai <ani.da.ds.ai@gmail.com>
2025-02-24 10:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import random
-import sklearn as sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
